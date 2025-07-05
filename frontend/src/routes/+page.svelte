3064dfd2a416a5055bebd3f1ee10027fd1f4242e
mihiryadav20 <80512042+mihiryadav20@users.noreply.github.com>
2025-07-05 21:57:50 +0530

    new staff login button

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -16,4 +16,14 @@
       <button on:click={() => goto('/login')} class="btn btn-primary p-4 mx-4">Get started</button>
     </div>
   </div>
+  
+  <!-- Staff login link in bottom right -->
+  <div class="fixed bottom-4 right-4">
+    <a href="/login" class="btn btn-ghost">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+      </svg>
+      Login as Staff
+    </a>
+  </div>
 </div>
